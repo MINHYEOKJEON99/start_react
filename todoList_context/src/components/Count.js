06a6e6451afa6f72e278f8fd920f06bda921c9b2
MINHYEOKJEON99 <133513404+MINHYEOKJEON99@users.noreply.github.com>
2023-09-27 13:26:08 +0900

    Update Count.js

--- todoList_context/src/components/Count.js
+++ todoList_context/src/components/Count.js
@@ -1,6 +1,6 @@
 import classes from "./Count.module.css";
 
 const Count = (props) => {
-  return <div className={classes.count}>{`할 일 ${props.remain}개남음`}</div>;
+  return <div className={classes.count}>{`할 일은 총${props.remain}개!`}</div>;
 };
 export default Count;
